0bd27caf3fbb15da62d79d079f624ca2f4c642a7
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-04-24 19:38:20 +0200

    cannot set if unset, because uvars were mostly somehow set to something empty

--- etc/fish/conf.d/20_env_update.fish
+++ etc/fish/conf.d/20_env_update.fish
@@ -1,6 +1,6 @@
 #!/bin/fish
 
 update_paenv
-var_Ux_set_from_config_file monitor_widths
-var_Ux_set_from_config_file screen_layout
-var_Ux_set_from_config_file screen_layout_v2
+env_set_from_config_file monitor_widths
+env_set_from_config_file screen_layout
+env_set_from_config_file screen_layout_v2
