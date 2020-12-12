9e97117488cd3fc2f73081dc68a5f5107883a7ca
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2020-12-12 15:11:09 +0100

    fix: do not mistakenly wipe out env vars if no file

--- etc/fish/conf.d/15_functions.fish
+++ etc/fish/conf.d/15_functions.fish
@@ -5,13 +5,15 @@ function x
 end
 
 function env_set_from_config_file -a name
-    set -q name
-    if test $status -eq 0
-            set fn "$EPREFIX"/etc/"$name"
-            test -e "$fn"
-            and set val (cat "$fn")
-        #sed_debug_flag_replace_line:echo set -gx "$name" "$val"
-        set -gx "$name" "$val"
+    if not set -q $name # do nothing if no var exists
+         set fn "$EPREFIX"/etc/"$name"
+         if test -e $fn  # do nothing if no file
+            set val (cat $fn)
+            if test -n "$fn"   # do nothing if empty string
+                #sed_debug_flag_replace_line:echo set -gx "$name" "$val"
+                set -gx "$name" "$val"
+            end
+        end
     end
 end
 
