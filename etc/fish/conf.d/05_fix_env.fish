08e9687816f826f2d0f18691860f669369a4379c
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2021-01-28 16:27:12 +0100

    add env fix because ssh is hard to configure

---
+++ etc/fish/conf.d/05_fix_env.fish
@@ -0,0 +1,6 @@
+if not test -e /usr/lib64/libswmhack.so
+    if set -q LD_PRELOAD
+        eval set LD_PRELOAD (string replace --all --regex \''[a-z/]+libswmhack[.]so[.0-9]*'\' \'\' \""$LD_PRELOAD"\")
+    end
+end
+
