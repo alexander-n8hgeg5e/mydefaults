87819f61c12877c85cd94078653d66c896439e9a
alexander-n8hgeg5e <alexander.w.email@gmail.com>
2022-04-29 17:24:43 +0200

    try fix fish env

--- etc/fish/conf.d/05_fix_env.fish
+++ etc/fish/conf.d/05_fix_env.fish
@@ -3,5 +3,10 @@ if not test -e /usr/lib64/libswmhack.so
         eval set LD_PRELOAD (string replace --all --regex \''[a-z/]+libswmhack[.]so[.0-9]*'\' \'\' \""$LD_PRELOAD"\")
     end
 end
+for var in (set -xng)
+    if not contains "$var" PWD SHLVL
+        set -x $var (string replace -r -- '[$]{([^}]+)}' '\1' "$$var" )
+    end
+end
 
 # vim: set syntax=sh :
